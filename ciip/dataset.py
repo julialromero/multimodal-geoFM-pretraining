2e030ae3db5490e4821dfac07d70510a71d07034
Isaiah Lyons-Galante <isaiah.lyons-galante@colorado.edu>
2024-08-07 17:03:29 -0600

    Add dataset file for ciiip.

--- ciip/dataset.py
+++ ciip/dataset.py
@@ -1,3 +1,278 @@
 # code for pytorch dataset class
 # load samples and ground truth label
 
+
+# from chatgpt
+# args.csv_img1_key = 'image1_path'
+# args.csv_img2_key = 'image2_path'
+# args.csv_label_key = 'label'
+# args.csv_separator = ','
+import ast
+import json
+import logging
+import math
+import os
+import random
+import sys
+from dataclasses import dataclass
+from multiprocessing import Value
+
+import numpy as np
+import pandas as pd
+import torch
+import torchvision.datasets as datasets
+import webdataset as wds
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler, IterableDataset, get_worker_info
+from torch.utils.data.distributed import DistributedSampler
+from webdataset.filters import _shuffle
+from webdataset.tariterators import base_plus_ext, url_opener, tar_file_expander, valid_sample
+
+try:
+    import horovod.torch as hvd
+except ImportError:
+    hvd = None
+
+
+class S12Dataset(Dataset):
+    def __init__(self, root, transforms, img1_key, img2_key, label_key, sep="\t"):
+        # logging.debug(f'Loading csv data from {input_filename}.')
+        # df = pd.read_csv(input_filename, sep=sep)
+
+        # self.img1_paths = df[img1_key].tolist()
+        # self.img2_paths = df[img2_key].tolist()
+        # self.labels = df[label_key].tolist()
+        # self.transforms = transforms
+        # 
+
+        # s2a->location id->season id->bands of the same image
+
+        self.root = root
+        self.num_locations = None
+        self.s1_paths = []
+        self.s2_paths = []
+
+        s1_dir = os.path.join(self.root, 's1')
+        s2_dir = os.path.join(self.root, 's2-l2a')
+
+        s1_samples = os.listdir(s1_dir)
+        s2_samples = os.listdir(s2_dir)
+        assert len(s1_samples) == len(s2_samples), 'Number of locations in S1 and S2 should be the same'
+        self.num_locations = len(s1_samples)
+        print(f'Number of locations in dataset: {self.num_locations}')
+
+        # location list
+        self.locations = s1_samples
+
+        # create path lists
+        s1_location_paths = [os.path.join(s1_dir, location_id) for location_id in self.locations]
+        s2_location_paths = [os.path.join(s2_dir, location_id) for location_id in self.locations]
+
+        # 
+
+        # 
+        # parse the date from the file name (morteza regex)
+
+        # sort the dates
+
+
+        # YYYYMMDD
+        # different seasons are negative samples 
+
+        logging.debug('Done loading data.')
+    
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        img1 = self.transforms(Image.open(str(self.img1_paths[idx])))
+        img2 = self.transforms(Image.open(str(self.img2_paths[idx])))
+        label = torch.tensor(self.labels[idx], dtype=torch.float32)
+        return (img1, img2), label
+
+
+class SharedEpoch:
+    def __init__(self, epoch: int = 0):
+        self.shared_epoch = Value('i', epoch)
+
+    def set_value(self, epoch):
+        self.shared_epoch.value = epoch
+
+    def get_value(self):
+        return self.shared_epoch.value
+
+
+@dataclass
+class DataInfo:
+    dataloader: DataLoader
+    sampler: DistributedSampler = None
+    shared_epoch: SharedEpoch = None
+
+    def set_epoch(self, epoch):
+        if self.shared_epoch is not None:
+            self.shared_epoch.set_value(epoch)
+        if self.sampler is not None and isinstance(self.sampler, DistributedSampler):
+            self.sampler.set_epoch(epoch)
+
+
+def expand_urls(urls, weights=None):
+    if weights is None:
+        expanded_urls = wds.shardlists.expand_urls(urls)
+        return expanded_urls, None
+    if isinstance(urls, str):
+        urllist = urls.split("::")
+        weights = weights.split('::')
+        assert len(weights) == len(urllist),\
+            f"Expected the number of data components ({len(urllist)}) and weights({len(weights)}) to match."
+        weights = [float(weight) for weight in weights]
+        all_urls, all_weights = [], []
+        for url, weight in zip(urllist, weights):
+            expanded_url = list(braceexpand.braceexpand(url))
+            expanded_weights = [weight for _ in expanded_url]
+            all_urls.extend(expanded_url)
+            all_weights.extend(expanded_weights)
+        return all_urls, all_weights
+    else:
+        all_urls = list(urls)
+        return all_urls, weights
+
+
+def get_dataset_size(shards):
+    shards_list, _ = expand_urls(shards)
+    dir_path = os.path.dirname(shards_list[0])
+    sizes_filename = os.path.join(dir_path, 'sizes.json')
+    len_filename = os.path.join(dir_path, '__len__')
+    if os.path.exists(sizes_filename):
+        sizes = json.load(open(sizes_filename, 'r'))
+        total_size = sum([int(sizes[os.path.basename(shard)]) for shard in shards_list])
+    elif os.path.exists(len_filename):
+        # FIXME this used to be eval(open(...)) but that seemed rather unsafe
+        total_size = ast.literal_eval(open(len_filename, 'r').read())
+    else:
+        total_size = None  # num samples undefined
+        # some common dataset sizes (at time of authors last download)
+        # CC3M (train): 2905954
+        # CC12M: 10968539
+        # LAION-400M: 407332084
+        # LAION-2B (english): 2170337258
+    num_shards = len(shards_list)
+    return total_size, num_shards
+
+
+def count_samples(dataloader):
+    os.environ["WDS_EPOCH"] = "0"
+    n_elements, n_batches = 0, 0
+    for (img1, img2), labels in dataloader:
+        n_batches += 1
+        n_elements += len(labels)
+        assert len(img1) == len(img2) == len(labels)
+    return n_elements, n_batches
+
+
+def log_and_continue(exn):
+    """Call in an exception handler to ignore any exception, issue a warning, and continue."""
+    logging.warning(f'Handling webdataset error ({repr(exn)}). Ignoring.')
+    return True
+
+
+def get_csv_dataset(args, preprocess_fn, is_train, epoch=0):
+    input_filename = args.train_data if is_train else args.val_data
+    assert input_filename
+    dataset = ImagePairDataset(
+        input_filename,
+        preprocess_fn,
+        img1_key=args.csv_img1_key,
+        img2_key=args.csv_img2_key,
+        label_key=args.csv_label_key,
+        sep=args.csv_separator,
+    )
+    num_samples = len(dataset)
+    sampler = DistributedSampler(dataset) if args.distributed and is_train else None
+    shuffle = is_train and sampler is None
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        shuffle=shuffle,
+        num_workers=args.workers,
+        pin_memory=True,
+        sampler=sampler,
+        drop_last=is_train,
+    )
+    dataloader.num_samples = num_samples
+    dataloader.num_batches = len(dataloader)
+
+    return DataInfo(dataloader, sampler)
+
+
+def get_synthetic_dataset(args, preprocess_fn, is_train, epoch=0):
+    class SyntheticImagePairDataset(Dataset):
+        def __init__(self, transform=None, image_size=(224, 224), dataset_size=100):
+            self.transform = transform
+            self.image_size = image_size
+            self.image = Image.new('RGB', image_size)
+            self.dataset_size = dataset_size
+
+        def __len__(self):
+            return self.dataset_size
+
+        def __getitem__(self, idx):
+            if self.transform is not None:
+                img1 = self.transform(self.image)
+                img2 = self.transform(self.image)
+            label = torch.tensor(1.0 if idx == 0 else 0.0, dtype=torch.float32)
+            return (img1, img2), label
+
+    image_size = preprocess_fn.transforms[0].size
+    dataset = SyntheticImagePairDataset(
+        transform=preprocess_fn, image_size=image_size, dataset_size=args.train_num_samples)
+    num_samples = len(dataset)
+    sampler = DistributedSampler(dataset) if args.distributed and is_train else None
+    shuffle = is_train and sampler is None
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        shuffle=shuffle,
+        num_workers=args.workers,
+        pin_memory=True,
+        sampler=sampler,
+        drop_last=is_train,
+    )
+    dataloader.num_samples = num_samples
+    dataloader.num_batches = len(dataloader)
+
+    return DataInfo(dataloader, sampler)
+
+
+def get_dataset_fn(data_path, dataset_type):
+    if dataset_type == "csv":
+        return get_csv_dataset
+    elif dataset_type == "synthetic":
+        return get_synthetic_dataset
+    elif dataset_type == "auto":
+        ext = data_path.split('.')[-1]
+        if ext in ['csv', 'tsv']:
+            return get_csv_dataset
+        else:
+            raise ValueError(
+                f"Tried to figure out dataset type, but failed for extension {ext}.")
+    else:
+        raise ValueError(f"Unsupported dataset type: {dataset_type}")
+    
+
+def get_data(args, preprocess_fns, epoch=0):
+    preprocess_train, preprocess_val = preprocess_fns
+    data = {}
+
+    if args.train_data or args.dataset_type == "synthetic":
+        data["train"] = get_dataset_fn(args.train_data, args.dataset_type)(
+            args, preprocess_train, is_train=True, epoch=epoch)
+
+    if args.val_data:
+        data["val"] = get_dataset_fn(args.val_data, args.dataset_type)(
+            args, preprocess_val, is_train=False)
+
+    return data
+
