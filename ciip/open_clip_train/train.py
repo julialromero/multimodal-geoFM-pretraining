55b16f7873bd7f5efdb0e8ceeecbb001d5852b01
invictusying <wangzy970301@gmail.com>
2025-02-19 18:03:41 -0700

    Fix distributed training.

--- ciip/open_clip_train/train.py
+++ ciip/open_clip_train/train.py
@@ -8,13 +8,17 @@ import sys
 import numpy as np
 import torch
 import torch.nn.functional as F
+from fiona.env import local
 from torch.nn.parallel.distributed import DistributedDataParallel
+from torch.xpu import device
 
 try:
     import wandb
 except ImportError:
     wandb = None
 
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+sys.path.insert(0, parent_dir)
 from open_clip import get_input_dtype
 from open_clip_train.distributed import is_master
 from open_clip_train.zero_shot import zero_shot_eval
